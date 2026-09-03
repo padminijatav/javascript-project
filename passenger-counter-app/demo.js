70b7109f361c7fe8cd5927ed75dae3b5d12e4258
padminijatav <pdmnjatav@gmail.com>
2026-09-04 00:13:31 +0530

    Initial commit

---
+++ passenger-counter-app/demo.js
@@ -0,0 +1,10 @@
+let welcomeEl=document.getElementById("welcome-el")
+
+let name ="per"
+
+let greeting="Welcome back "
+
+
+let welcomeMessage=greeting + name + "!"
+
+welcomeEl.innerText=welcomeMessage
